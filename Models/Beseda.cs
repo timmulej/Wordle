503e5c391c6cb9bda8a22fc1444b62ca15ee793d
timmulej <54589313+timmulej@users.noreply.github.com>
2022-05-18 14:46:26 +0200

    Model beseda

---
+++ Models/Beseda.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wordle.Models
+{
+    public class Beseda
+    {
+        [Required, MaxLength(1)]
+        public string Prva { get; set; }
+
+        public Level PrviLevel { get; set; }
+
+        [Required, MaxLength(1)]
+        public string Druga { get; set; }
+
+        public Level DrugiLevel { get; set; }
+
+        [Required, MaxLength(1)]
+        public string Tretja { get; set; }
+
+        public Level tretjiLevel { get; set; }
+
+        [Required, MaxLength(1)]
+        public string Cetrta { get; set; }
+
+        public Level CetrtiLevel { get; set; }
+
+        [Required, MaxLength(1)]
+        public string Peta { get; set; }
+         
+        public Level PetiLevel { get; set; }
+
+        public double ExpectedInformation { get; set; }
+
+        public double Information { get; set; }
+
+    }
+
+    public enum Level { Siva, Rumena, Zelena }
+}
+
